fd0bf67cdc33f900be020b8c2f9779d33430bbd9
Jacob Adams <jacob.sc.adams@gmail.com>
2025-05-08 15:21:26 -0700

    Add profile picture handling and update user info display logic

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const URL = "https://api.github.com/";
 
 let usernameInput = document.getElementById("usernameInput");
 let submitButton = document.querySelector(".submit-button");
+let profileContainer = document.querySelector(".profile-container");
 let currUserData;
 
 async function pullUserInfo(username) {
@@ -22,6 +23,15 @@ async function pullUserInfo(username) {
   }
 }
 
+// Pulls user profile picture, creates an <img> element and adds it to the DOM
+async function addUserProfilePhoto(avatarUrl) {
+  const imgElement = document.createElement("img");
+  imgElement.classList.add("profile-picture");
+  if (imgElement) imgElement.src = avatarUrl;
+
+  profileContainer.appendChild(imgElement);
+}
+
 async function handleClick() {
   const username = usernameInput.value.trim();
   usernameInput.value = "";
@@ -33,6 +43,15 @@ async function handleClick() {
 
   if (currUserData) {
     console.log("User login:", currUserData.login);
+    const { avatar_url, bio, html_url, following_url, repos_url, name, login } =
+      currUserData;
+
+    console.log(`Bio: ${bio}`);
+    console.log(`Avatar URL: ${avatar_url}`);
+    await addUserProfilePhoto(avatar_url);
+  } else {
+    console.error("Unable to pull user data");
+    return;
   }
 }
 
